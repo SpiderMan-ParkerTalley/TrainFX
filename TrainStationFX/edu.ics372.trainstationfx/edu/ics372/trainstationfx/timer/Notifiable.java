2746e7ebfc5ef0f1d5b5032e3658f7df79e970f7
Jim Sawicki <77102469+dadPower@users.noreply.github.com>
2022-04-10 17:48:53 -0500

    Testing Github Desktop
    Testing Github Desktop commit. Added meat to Notifiable Interface.

--- TrainStationFX/edu.ics372.trainstationfx/edu/ics372/trainstationfx/timer/Notifiable.java
+++ TrainStationFX/edu.ics372.trainstationfx/edu/ics372/trainstationfx/timer/Notifiable.java
@@ -1,5 +1,14 @@
 package edu.ics372.trainstationfx.timer;
 
 public interface Notifiable {
+	/**
+	 * Process timer ticks
+	 */
+	public void OnTimerTick(int timerValue);
+
+	/**
+	 * Process timer runs out event
+	 */
+	public void onTimerRunsOut();
 
 }
--- TrainStationFX/edu.ics372.trainstationfx/module-info.java
+++ TrainStationFX/edu.ics372.trainstationfx/module-info.java
@@ -2,7 +2,7 @@ module edu.ics372.trainstationfx {
 	requires javafx.controls;
 	requires javafx.base;
 	requires java.desktop;
-	
+
 	exports edu.ics372.trainstationfx.display to javafx.graphics;
 	exports edu.ics372.trainstationfx.start to javafx.graphics;
 }
