4c9bd8391353aa969dcb8b1e1362a8179e317628
Jim Sawicki <77102469+dadPower@users.noreply.github.com>
2022-04-13 15:09:32 -0500

    small bump

--- TrainStationFX/edu.ics372.trainfx/edu/ics372/trainfx/display/TrainDisplay.java
+++ TrainStationFX/edu.ics372.trainfx/edu/ics372/trainfx/display/TrainDisplay.java
@@ -1,7 +1,8 @@
 package edu.ics372.trainfx.display;
 
 /**
- * Lays out the bare minimum that the display should be able to do.
+ * Lays out the bare minimum that the display should be able to do. Not finished
+ * maybe.
  * 
  * @author Jim Sawicki TODO: MARK UNTIL CONFIRMED FINISHED
  *
