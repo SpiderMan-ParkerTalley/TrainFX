349a69b0d1113606477afd5cb76d3adde1a2ff5c
Jim Sawicki <77102469+dadPower@users.noreply.github.com>
2022-04-13 11:29:41 -0500

    Buttons and Display
    Finished GUIButton, Obstruction Button.
    Started GUIDisplay, TrainDisplay

--- TrainStationFX/edu.ics372.trainfx/edu/ics372/trainfx/buttons/GUIButton.java
+++ TrainStationFX/edu.ics372.trainfx/edu/ics372/trainfx/buttons/GUIButton.java
@@ -1,5 +1,25 @@
 package edu.ics372.trainfx.buttons;
 
-public class GUIButton {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 
+/**
+ * Our very own abstract GUI button class.
+ * 
+ * @author Jim Sawicki
+ * 
+ */
+public abstract class GUIButton extends Button implements EventHandler<ActionEvent> {
+
+	/**
+	 * Create the button with the proper text. Makes the button a listener to clicks
+	 * on itself.
+	 * 
+	 * @param string the text
+	 */
+	public GUIButton(String string) {
+		super(string);
+		setOnAction(this);
+	}
 }
--- TrainStationFX/edu.ics372.trainfx/module-info.java
+++ TrainStationFX/edu.ics372.trainfx/module-info.java
@@ -2,6 +2,7 @@ module edu.ics372.trainstationfx {
 	requires javafx.controls;
 	requires javafx.base;
 	requires java.desktop;
+	requires transitive javafx.graphics;
 
 	exports edu.ics372.trainfx.display to javafx.graphics;
 	exports edu.ics372.trainfx.start to javafx.graphics;
