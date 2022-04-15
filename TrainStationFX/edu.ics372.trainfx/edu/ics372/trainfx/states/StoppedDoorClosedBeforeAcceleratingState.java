3fe3aeac8e0ec684f1526ca19c00f6380a182819
Jim Sawicki <77102469+dadPower@users.noreply.github.com>
2022-04-14 20:56:00 -0500

    Revert "made some changes to main etc"

--- TrainStationFX/edu.ics372.trainfx/edu/ics372/trainfx/states/StoppedDoorClosedBeforeAcceleratingState.java
+++ TrainStationFX/edu.ics372.trainfx/edu/ics372/trainfx/states/StoppedDoorClosedBeforeAcceleratingState.java
@@ -1,50 +1,5 @@
 package edu.ics372.trainfx.states;
 
-import edu.ics372.trainfx.timer.Notifiable;
-import edu.ics372.trainfx.timer.Timer;
+public class StoppedDoorClosedBeforeAcceleratingState {
 
-/**
- * Represents the train state when it is stopped, the doors are already closed,
- * and it is waiting to begin the acceleration state.
- * 
- * @author Jim Sawicki
- *
- */
-public class StoppedDoorClosedBeforeAcceleratingState extends TrainState implements Notifiable {
-
-	private static StoppedDoorClosedBeforeAcceleratingState instance;
-	private Timer timer;
-
-	/**
-	 * Singleton Pattern
-	 */
-	private StoppedDoorClosedBeforeAcceleratingState() {
-
-	}
-
-	public static StoppedDoorClosedBeforeAcceleratingState getInstance() {
-		if (instance == null) {
-			instance = new StoppedDoorClosedBeforeAcceleratingState();
-		}
-		return instance;
-	}
-
-	@Override
-	public void enter() {
-		System.out.println("ENTERED SDCBA"); // TODO debug
-		timer = new Timer(this, 3);
-		TrainContext.getInstance().showStoppedDoorClosedBeforeAcceleratingState();
-		TrainContext.getInstance().showTimeLeft(timer.getTimeValue());
-	}
-
-	@Override
-	public void leave() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void OnTimerTick(int timerValue) {
-		TrainContext.getInstance().showTimeLeft(timerValue);
-	}
 }
